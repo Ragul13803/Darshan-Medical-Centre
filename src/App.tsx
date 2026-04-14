9dacdbdfdcb4742c4e93a70d6a6d97282ff380d2
Ragul <155870749+Ragul13803@users.noreply.github.com>
2026-04-14 22:55:13 +0530

    Comment out unused imports and state
    Comment out unused imports and state management.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
-import { Router, Routes, Route, BrowserRouter } from 'react-router-dom'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Layout from './layout/Layout'
 import Home from './pages/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <BrowserRouter>
